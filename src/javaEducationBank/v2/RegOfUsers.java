ef837522aed439574ebd656e3a064a45f2b3e193
Andrey Bayshev <bayshev21@gmail.com>
2019-06-10 14:20:43 +0300

    corrected register of users(class distribution)

---
+++ src/javaEducationBank/v2/RegOfUsers.java
@@ -0,0 +1,22 @@
+package javaEducationBank.v2;
+
+public class RegOfUsers {
+
+    public User usr(User[] users, String login, User user1){
+        int i;
+
+        for (i = 0; i < users.length; i++) {
+            user1 = new User();
+            if (users[i].getLogin().equals(login)) {
+                return null;
+            } else {
+
+                user1.setLogin(login);
+            }
+        }
+
+        return user1;
+    }
+
+
+}
